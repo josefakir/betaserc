1c716ebeb3b6edd685f569bd5c2e7d48a6500ee6
Pepe Becerra <pepe@Pepes-MacBook-Pro.local>
2015-12-13 19:11:28 -0600

    primer slide

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+function mostrar1(){
+	$('#s2_logo').fadeIn('slow',function(){
+		$('#s2_t1').fadeIn(1000,function(){
+			$('#s2_t2').fadeIn(1000,function(){
+				$('#s2_chica').fadeIn(1000);
+			});
+		 });
+	});
+}
+function ocultar1(){
+	$('#s2_logo').fadeOut('fast');
+	$('#s2_t1').fadeOut('fast');
+	$('#s2_t2').fadeOut('fast');
+	$('#s2_chica').fadeOut('fast');
+}
 $(document).ready(function(){
 	$('#splash').fadeIn('slow');
 	$(".item").swipe({
@@ -10,9 +25,35 @@ $(document).ready(function(){
 	    			$('#itemcontainer').animate({
 		    			left : margen + 'px'
 		    		},function(){
+		    			
 		    			switch(slide){
-		    				case 1:
-		    					alert(slide)
+		    				case '1':
+		    					console.log(slide);
+		    					mostrar1();
+		    				break;
+		    				case '2':
+		    					console.log(slide);
+		    				break;
+		    				case '3':
+		    					console.log(slide);
+		    				break;
+		    				case '4':
+		    					console.log(slide);
+		    				break;
+		    				case '5':
+		    					console.log(slide);
+		    				break;
+		    				case '6':
+		    					console.log(slide);
+		    				break;
+		    				case '7':
+		    					console.log(slide);
+		    				break;
+		    				case '8':
+		    					console.log(slide);
+		    				break;
+		    				case '9':
+		    					console.log(slide);
 		    				break;
 		    			}
 		    		});
