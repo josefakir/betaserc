3eac02d2b31e2a94fdfbb3a96a723757b30719d8
Pepe Becerra <pepe@Pepes-MacBook-Pro.local>
2015-12-24 10:23:45 -0700

    Animaciones

--- js/main.js
+++ js/main.js
@@ -7,6 +7,65 @@ function mostrar1(){
 		 });
 	});
 }
+function mostrar2(){
+	$('#s3_1').fadeIn('slow',function(){
+		$('#s3_2').fadeIn(1000,function(){
+			$('#s3_3').fadeIn(1000,function(){
+				$('#s3_4').fadeIn(1000);
+			});
+		 });
+	});
+}
+function mostrar3(){
+	$('#s4_1').fadeIn('slow',function(){
+		$('#s4_2').fadeIn(1000,function(){
+			$('#s4_3').fadeIn(1000,function(){
+				$('#s4_4').fadeIn(1000);
+			});
+		 });
+	});
+}
+function mostrar4(){
+	$('#s5_1').fadeIn('slow',function(){
+		$('#s5_2').fadeIn(1000,function(){
+			$('#s5_3').fadeIn(1000,function(){
+				$('#s5_4').fadeIn(1000);
+			});
+		 });
+	});
+}
+function mostrar5(){
+	$('#s6_1').fadeIn('slow',function(){
+		$('#s6_2').fadeIn(1000,function(){
+			$('#s6_3').fadeIn(1000,function(){
+				$('#s6_4').fadeIn(1000);
+			});
+		 });
+	});
+}
+function mostrar6(){
+	$('#s7_1').fadeIn('slow',function(){
+		$('#s7_2').fadeIn(1000,function(){
+			$('#s7_3').fadeIn(1000);
+		 });
+	});
+}
+function mostrar7(){
+	$('#s8_1').fadeIn('slow',function(){
+		$('#s8_2').fadeIn(1000,function(){
+			$('#s8_3').fadeIn(1000,function(){
+				$('#s8_4').fadeIn(1000);
+			});
+		 });
+	});
+}
+function mostrar8(){
+	$('#s9_1').fadeIn('slow',function(){
+		$('#s9_2').fadeIn(1000,function(){
+			$('#s9_3').fadeIn(1000);
+		 });
+	});
+}
 function ocultar1(){
 	$('#s2_logo').fadeOut('fast');
 	$('#s2_t1').fadeOut('fast');
@@ -33,23 +92,30 @@ $(document).ready(function(){
 		    				break;
 		    				case '2':
 		    					console.log(slide);
+		    					mostrar2();
 		    				break;
 		    				case '3':
+		    					mostrar3();
 		    					console.log(slide);
 		    				break;
 		    				case '4':
+		    					mostrar4();
 		    					console.log(slide);
 		    				break;
 		    				case '5':
+		    					mostrar5();
 		    					console.log(slide);
 		    				break;
 		    				case '6':
+		    					mostrar6();
 		    					console.log(slide);
 		    				break;
 		    				case '7':
+		    					mostrar7();
 		    					console.log(slide);
 		    				break;
 		    				case '8':
+		    					mostrar8();
 		    					console.log(slide);
 		    				break;
 		    				case '9':
